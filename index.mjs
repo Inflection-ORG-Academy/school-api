9d062b5b8b0632d561720cee9f2bc3b620d643f2
Prakhar Maurya <000.excel@gmail.com>
2024-02-15 11:25:29 +0530

    global undefined route capture

--- index.mjs
+++ index.mjs
@@ -12,7 +12,6 @@ const port = 3000
 const pgClient = new pg.Client(process.env.DB_URL)
 
 app.get('/', errorCapture((req, res) => {
-  throw Error("jsdhvghjbvskdns")
   res.send("hello")
 }))
 
@@ -25,6 +24,10 @@ app.get('/admissions', errorCapture(async (req, res) => {
   // SQL
   const data = await pgClient.query('SELECT admissions.id AS admission_id,student_id,name,father_name,class from admissions INNER JOIN students ON admissions.student_id = students.id')
   res.json(data.rows)
+}))
+
+app.all('*', errorCapture(async (req, res) => {
+  throw Error("route not exists")
 }))
 
 app.listen(port, async () => {
