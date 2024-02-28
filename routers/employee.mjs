a655386a769745bef7c83d8ac93327bac8a1e3b6
Prakhar Maurya <000.excel@gmail.com>
2024-02-28 12:16:55 +0530

    home work upload profile and get profile photo

--- routers/employee.mjs
+++ routers/employee.mjs
@@ -1,8 +1,10 @@
 import express from "express"
+import * as path from 'path';
 import { errorCapture } from "./error.mjs"
 import { pgClient } from "../database.mjs"
 import jwt from "jsonwebtoken"
 import { upload } from "../multer.mjs"
+import { authentication } from "../middleware/auth.mjs"
 
 const employeeRouter = express.Router()
 
@@ -22,11 +24,18 @@ employeeRouter.post("/login", errorCapture(async (req, res, next) => {
   res.json({ token })
 }))
 
-employeeRouter.post("/upload", errorCapture(upload.single("image")), errorCapture(async (req, res, next) => {
-
+employeeRouter.post("/upload", authentication, errorCapture(upload.single("image")), errorCapture(async (req, res, next) => {
+  // todo: save file name to employee profile photo in column name profile_photo
+  console.log(req.file)
   res.json({
     message: "file uploaded successfully"
   })
+}))
+
+employeeRouter.get('/myprofile_photo', errorCapture(function (req, res, next) {
+  // TODO: get file name from DB
+  const fileName = "apple.jpg"
+  res.sendFile(path.resolve(`uploads/${fileName}`));
 }))
 
 export { employeeRouter }
