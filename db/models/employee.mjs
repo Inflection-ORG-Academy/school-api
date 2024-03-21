5c6138814b10d746bbde1628d849d4fb28e4a709
Prakhar Maurya <000.excel@gmail.com>
2024-03-21 09:40:20 +0530

    student models created

--- db/models/employee.mjs
+++ db/models/employee.mjs
@@ -1,20 +1,20 @@
 import { bigint } from "drizzle-orm/mysql-core";
 import { pgTable, bigserial, text, timestamp, pgEnum } from "drizzle-orm/pg-core";
 
-const roles = pgEnum('roles', ['worker', 'clerk', 'principal', 'teacher', 'accountant', 'admin', 'marketer'], { mode: "string" });
+const Roles = pgEnum('roles', ['worker', 'clerk', 'principal', 'teacher', 'accountant', 'admin', 'marketer']);
 
 const Employee = pgTable('employees', {
-  id: bigserial('id', { mode: "number" }).primaryKey(),
+  id: bigserial('id', { mode: "bigint" }).primaryKey(),
   name: text('name').notNull(),
   email: text('email').unique().notNull(),
   phone: text('phone').notNull(),
-  password: text('password').notNull(),
-  role: roles('roles').notNull(),
+  password: text('password').notNull(), //$2b$10$xUNmNw6dtZoy5.v0lcQleuzjfQe6M2WskBoq8.ssXFWDYRFtmrppS // 1234
+  role: Roles('roles').notNull(),
   profilePhoto: text('profile_photo'),
   forgotToken: text('forgot_token'),
   forgotTokenCreatedAt: timestamp('forgot_token_created_at', { precision: 0, withTimezone: true }),
   createdAt: timestamp('created_at', { precision: 0, withTimezone: true }).notNull().default('now()'),
-  createdBy: bigint('created_by', { mode: "number" }).references(() => Employee.id),
+  createdBy: bigint('created_by', { mode: "bigint" }).references(() => Employee.id),
 });
 
-export { Employee, roles }
+export { Employee, Roles }
---
+++ db/models/student.mjs
@@ -0,0 +1,41 @@
+import { bigint } from "drizzle-orm/mysql-core";
+import { pgTable, bigserial, text, timestamp, pgEnum, date } from "drizzle-orm/pg-core";
+
+const Genders = pgEnum('genders', ['male', 'female']);
+const Categories = pgEnum('categories', ['gen', 'obc', 'sc', 'st']);
+
+const Student = pgTable('students', {
+  id: bigserial('id', { mode: "bigint" }).primaryKey(),
+  registrationId: text('registration_id').unique().notNull(),
+  name: text('name').notNull(),
+  password: text('password').notNull(),
+  forgotToken: text('forgot_token'),
+  forgotTokenCreatedAt: timestamp('forgot_token_created_at', { precision: 0, withTimezone: true }),
+  createdAt: timestamp('created_at', { precision: 0, withTimezone: true }).notNull().default('now()'),
+});
+
+const Registration = pgTable('students_registration', {
+  id: bigserial('id', { mode: "number" }).primaryKey(),
+  studentId: bigint('student_id', { mode: "bigint" }).notNull().references(() => Student.id),
+  fatherName: text('father_name').notNull(),
+  phone: text('phone').notNull(),
+  alterPhone: text('alter_phone'),
+  category: Categories('category'),
+  email: text('email'),
+  religion: text('religion'),
+  address: text('address').notNull(),
+  aadhar: bigint('aadhar', { mode: "bigint" }),
+  nationality: text('nationality'),
+  dob: date('dob').notNull(),
+  motherName: text('mother_name'),
+  gender: text('gender'),
+  note: text('note'),
+  photoUrl: text('photo_url'),
+  signUrl: text('sign_url'),
+  aadharUrl: text('aadhar_url'),
+  lastYearResultUrl: text('last_year_result_url'),
+  tc: text('tc'),
+  cc: text('cc'),
+});
+
+export { Student, Registration, Genders, Categories }
